a26dd0818f3be3679570eef2a9aaef9147019675
waykwo <72991933+waykwo@users.noreply.github.com>
2024-10-14 20:30:25 -0400

    Add logic for first guess

--- src/WordlePage.jsx
+++ src/WordlePage.jsx
@@ -1,17 +1,33 @@
+import { useState } from "react";
+
 export function WordlePage() {
-  console.log("Welcome to Wordle!")
+  // console.log("Welcome to Wordle!")
   const words = ["Flame"];
-  const word_of_day = words[0];
+  const wordOfDay = words[0];
+  const [guess1, setGuess1] = useState("");
+  let guess1Split;
+
+  if (guess1 == wordOfDay) {
+    console.log("Congratulations!");
+  } else {
+    console.log("Try Again!");
+    // console.log(guess1.split(""));
+    guess1Split = guess1.split("");
+    console.log(guess1Split);
+    console.log(guess1Split[0]);
+  }
+
   return (
     <main>
       <h1>Welcome to Wordle!</h1>
       <form action="">
         Guess 1:
         <input type="text"
-        name="guess_1"
+        name="guess1"
+        maxLength={5}
         onChange={(event) => setGuess1(event.target.value)}/>
       </form>
-      <h3>{guess_1}</h3>
+      <h3>{guess1}</h3>
     </main>
   )
 }
