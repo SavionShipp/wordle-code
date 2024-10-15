4a79a63bc1ac19042f9b4b443a709af688d9cd0b
waykwo <72991933+waykwo@users.noreply.github.com>
2024-10-14 20:09:56 -0400

    Begin guess form

--- src/WordlePage.jsx
+++ src/WordlePage.jsx
@@ -6,8 +6,12 @@ export function WordlePage() {
     <main>
       <h1>Welcome to Wordle!</h1>
       <form action="">
-        Guess 1: <input type="text" name="guess_1" onChange={}/>
+        Guess 1:
+        <input type="text"
+        name="guess_1"
+        onChange={(event) => setGuess1(event.target.value)}/>
       </form>
+      <h3>{guess_1}</h3>
     </main>
   )
 }
